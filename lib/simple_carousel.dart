9a6e6f924f2509d1a2cbbcba295bb9b2a8274e99
Fabio paz <fabio.paz@reservafacil.tur.br>
2020-10-18 12:35:47 -0300

    first commit

---
+++ lib/simple_carousel.dart
@@ -0,0 +1,17 @@
+library simple_carousel;
+
+import 'package:flutter/material.dart';
+
+class SimpleCarousel extends StatefulWidget {
+  SimpleCarousel({Key key}) : super(key: key);
+
+  @override
+  _SimpleCarousel createState() => _SimpleCarousel();
+}
+
+class _SimpleCarousel extends State<SimpleCarousel> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
